d67521ff56265d4d75225002a877461c027481ae
Maru Amallo <amamarul@gmail.com>
2021-03-20 18:52:31 -0300

    feat: 3 - x to exit
    Lea las teclas presionadas y finalice al presionar la tecla “X”.

--- Program.cs
+++ Program.cs
@@ -8,36 +8,50 @@ namespace LaConsola
         {
             Uno();
             Dos();
+            Tres();
         }
 
         private static void Uno() 
         {
+            Console.Clear();
+            
             Console.WriteLine("Hola Mundo");
-            PresionaUnaTeclaParaSalir("Presiona una tecla para continuar");
+            PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
         }
 
         private static void Dos() 
         {
-            string nombre = IngresoTexto("\tPor favor ingrese su nombre\n");
             Console.Clear();
 
-            Console.WriteLine("\n\nBienvenid@ " + nombre);
-            
-            PresionaUnaTeclaParaSalir("Presiona una tecla para salir");
+            string nombre = IngresoTexto("Por favor ingrese su nombre");
+
+            Console.Clear();
+
+            Console.WriteLine("\n\nBienvenid@ " + nombre + "\n");
+
+            PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
+        }
+
+        private static void Tres() 
+        {           
+            PresionaXParaSalir();
         }
 
         public static void WriteGreenLine(string value)
         {
+            Console.Clear();
+            
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Black;
             
-            Console.WriteLine("\n\n");
-
+            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
             Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
+            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
 
             Console.ResetColor();
 
-            Console.WriteLine("\n");
+            Console.WriteLine("");
+            
         }
 
         public static string IngresoTexto(string mensaje = "Ingrese un texto")
@@ -52,16 +66,51 @@ namespace LaConsola
             return texto;
         }
 
-        public static void PresionaUnaTeclaParaSalir(string mensaje)
+        public static void PresionaUnaTeclaParaContinuar(string mensaje = "\n\nPresiona una tecla para continuar")
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             
-            Console.WriteLine("\n\n");
             Console.WriteLine(mensaje);
 
             Console.ResetColor();
 
             Console.ReadKey();
+        }
+
+        public static void PresionaXParaSalir(string tecla = "x")
+        {
+            bool valor;
+            bool continuar;
+            string Mensaje = "Presiona la tecla " + tecla + " para salir";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n"+Mensaje);
+            do
+            {
+
+                continuar = !bool.TryParse(
+                    Console.ReadLine()
+                            .ToLower()
+                            .Replace(tecla, "true"),
+                    out valor);
+                if (continuar)
+                {
+                    Console.WriteLine(Mensaje);
+                }
+            } while (continuar);
+
+            Console.ResetColor();
+            
+            Exit();
+        }
+
+        public static void Exit()
+        {
+            Console.Clear();
+
+            Console.WriteLine("\n\n\tHasta pronto!\n");
+
+            System.Environment.Exit(0);
         }
     }
 }
