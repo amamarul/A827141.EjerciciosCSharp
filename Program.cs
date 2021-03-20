66cd2f264cc197208c1835b960e4edf7da14818a
Maru Amallo <amamarul@gmail.com>
2021-03-20 18:03:39 -0300

    feat: 2  - ask name
    Solicite el ingreso de un texto cualquiera
    y lo presente nuevamente al usuario.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,61 @@ namespace LaConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Uno();
+            Dos();
+        }
+
+        private static void Uno() 
+        {
+            Console.WriteLine("Hola Mundo");
+            PresionaUnaTeclaParaSalir("Presiona una tecla para continuar");
+        }
+
+        private static void Dos() 
+        {
+            string nombre = IngresoTexto("\tPor favor ingrese su nombre\n");
+            Console.Clear();
+
+            Console.WriteLine("\n\nBienvenid@ " + nombre);
+            
+            PresionaUnaTeclaParaSalir("Presiona una tecla para salir");
+        }
+
+        public static void WriteGreenLine(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
+
+            Console.ResetColor();
+
+            Console.WriteLine("\n");
+        }
+
+        public static string IngresoTexto(string mensaje = "Ingrese un texto")
+        {
+            string texto;
+            do
+            {
+                WriteGreenLine(mensaje);
+                texto = Console.ReadLine();
+            } while (texto == "");
+
+            return texto;
+        }
+
+        public static void PresionaUnaTeclaParaSalir(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            Console.WriteLine("\n\n");
+            Console.WriteLine(mensaje);
+
+            Console.ResetColor();
+
             Console.ReadKey();
         }
     }
