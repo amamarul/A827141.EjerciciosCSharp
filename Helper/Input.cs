a4f6bcd709fb7be0b25573615acb210325b70f8e
Maru Amallo <amamarul@gmail.com>
2021-03-21 18:52:31 -0300

    feat: ADD Helper library

---
+++ Helper/Input.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Helper
+{
+    public static class Input
+    {
+        public static string IngresoTexto(string mensaje = "Ingrese un texto")
+        {
+            string texto;
+            do
+            {
+                Input.WriteGreenLine(mensaje);
+                texto = Console.ReadLine();
+            } while (texto == "");
+
+            return texto;
+        }
+
+        public static void WriteGreenLine(string value, bool extraLine = true)
+        {           
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
+            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
+
+            Console.ResetColor();
+
+            if (extraLine)
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        public static void WriteRedLine(string Mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Mensaje);
+            Console.ResetColor();
+        }
+
+        public static void PresionaUnaTeclaParaContinuar(string mensaje = "\n\nPresiona una tecla para continuar")
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            Console.WriteLine(mensaje);
+
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+
+        /*
+            Ref.: https://docs.microsoft.com/es-es/dotnet/api/system.console.keyavailable?view=net-5.0#System_Console_KeyAvailable
+         */
+        public static void PresionaXParaSalir(string tecla = "X", string Mensaje = "Presiona la tecla X para salir")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n");
+            
+            ConsoleKeyInfo cki;
+
+            ConsoleKey ck = GetConsoleKeyFromString(tecla);
+
+            do
+            {
+                Console.WriteLine(Mensaje);
+
+                cki = Console.ReadKey(true);
+            } while (cki.Key != ck);
+
+            Console.ResetColor();
+        }
+
+        /*
+            Ref.: https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=net-5.0
+         */
+        public static void SalirControlMas(string teclaSalida)
+        {
+            ConsoleKeyInfo cki;
+            // Prevent example from ending if CTL+C is pressed.
+            // Console.TreatControlCAsInput = true;
+
+            string salir = "CTL+" + teclaSalida.ToUpper();
+            string tempsalir = "";
+
+            Input.WriteRedLine("Presiona CTL + "+ teclaSalida +" para salir");
+
+            do {
+                cki = Console.ReadKey(true);
+
+                tempsalir = "";
+
+                if ((cki.Modifiers & ConsoleModifiers.Control) != 0) {
+                    tempsalir += "CTL+";
+                } 
+                    
+                tempsalir += cki.Key;
+                
+            } while (salir != tempsalir);
+        }
+
+
+        /*
+            Ref.: https://stackoverflow.com/a/11495179
+         */
+        private static ConsoleKey GetConsoleKeyFromString(string tecla)
+        {
+            ConsoleKey ck;
+
+            Enum.TryParse<ConsoleKey>(tecla, out ck);
+
+            return ck;
+        }
+
+        public static void Exit(string NombreUsuario)
+        {
+            Console.Clear();
+
+            Console.WriteLine("\n\n\tHasta pronto {0}!\n", NombreUsuario);
+
+            System.Environment.Exit(0);
+        }
+    }
+}
--- LaConsola/Program.cs
+++ LaConsola/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Threading;
 using LaConsola.Model;
+using Helper;
 
 namespace LaConsola
 {
@@ -24,33 +25,34 @@ namespace LaConsola
             Console.Clear();
             
             Console.WriteLine("Hola Mundo");
-            PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
+            Input.PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
         }
 
         private static void Dos() 
         {
             Console.Clear();
 
-            NombreUsuario = IngresoTexto("Por favor ingrese su nombre");
+            NombreUsuario = Input.IngresoTexto("Por favor ingrese su nombre");
 
             Console.Clear();
 
             Console.WriteLine("\n\nBienvenid@ " + NombreUsuario + "\n");
 
-            PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
+            Input.PresionaUnaTeclaParaContinuar("Presiona una tecla para continuar");
         }
 
         private static void Tres() 
         {           
-            PresionaXParaSalir();
+            Input.PresionaXParaSalir();
+            Input.Exit(NombreUsuario);
         }
 
         private static void Cuatro() 
         {           
             Console.Clear();
 
-            WriteGreenLine("          Momento de reflexion");
-            WriteGreenLine("¿En que estas pensando?");
+            Input.WriteGreenLine("          Momento de reflexion");
+            Input.WriteGreenLine("¿En que estas pensando?");
 
             IngresoFrases();
 
@@ -59,71 +61,8 @@ namespace LaConsola
 
         private static void Cinco() 
         {           
-            SalirControlMas("F");
-        }
-
-        public static void WriteGreenLine(string value, bool extraLine = true)
-        {           
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-            
-            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
-            Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
-            Console.WriteLine("".PadRight(Console.WindowWidth - 1));
-
-            Console.ResetColor();
-
-            if (extraLine)
-            {
-                Console.WriteLine("");
-            }
-        }
-
-        public static string IngresoTexto(string mensaje = "Ingrese un texto")
-        {
-            string texto;
-            do
-            {
-                WriteGreenLine(mensaje);
-                texto = Console.ReadLine();
-            } while (texto == "");
-
-            return texto;
-        }
-
-        public static void PresionaUnaTeclaParaContinuar(string mensaje = "\n\nPresiona una tecla para continuar")
-        {
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            
-            Console.WriteLine(mensaje);
-
-            Console.ResetColor();
-
-            Console.ReadKey();
-        }
-
-        /*
-            Ref.: https://docs.microsoft.com/es-es/dotnet/api/system.console.keyavailable?view=net-5.0#System_Console_KeyAvailable
-         */
-        public static void PresionaXParaSalir(string tecla = "X", string Mensaje = "Presiona la tecla X para salir")
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n");
-            
-            ConsoleKeyInfo cki;
-
-            ConsoleKey ck = GetConsoleKeyFromString(tecla);
-
-            do
-            {
-                Console.WriteLine(Mensaje);
-
-                cki = Console.ReadKey(true);
-            } while (cki.Key != ck);
-
-            Console.ResetColor();
-
-            Exit();
+            Input.SalirControlMas("F");
+            Input.Exit(NombreUsuario);
         }
 
         public static void IngresoFrases(
@@ -138,7 +77,7 @@ namespace LaConsola
             {
                 Console.WriteLine("\t" + Mensaje);
 
-                WriteRedLine("\to \""+ salida +"\" para no ingresar mas frases");
+                Input.WriteRedLine("\to \""+ salida +"\" para no ingresar mas frases");
 
                 input = Console.ReadLine();
 
@@ -147,71 +86,6 @@ namespace LaConsola
                     Frases.Add(new Frase(input));
                 }
             } while (salida != input);
-        }
-
-        /*
-            Ref.: https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=net-5.0
-         */
-        public static void SalirControlMas(string teclaSalida)
-        {
-            ConsoleKeyInfo cki;
-            // Prevent example from ending if CTL+C is pressed.
-            // Console.TreatControlCAsInput = true;
-
-            string salir = "SHIFT+CTL+" + teclaSalida.ToUpper();
-            string tempsalir = "";
-
-            WriteRedLine("Presiona CTL + "+ teclaSalida +" para salir");
-
-            do {
-                cki = Console.ReadKey(true);
-
-                tempsalir = "";
-
-                if((cki.Modifiers & ConsoleModifiers.Shift) != 0) {
-                    tempsalir += "SHIFT+";
-                    Console.Write("SHIFT+");
-                }
-
-                if ((cki.Modifiers & ConsoleModifiers.Control) != 0) {
-                    tempsalir += "CTL+";
-                    Console.Write("CTL+");
-                } 
-                    
-                tempsalir += cki.Key;
-                Console.WriteLine(cki.Key);
-                
-            } while (salir != tempsalir);
-
-            Exit();
-        }
-
-        private static void WriteRedLine(string Mensaje)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(Mensaje);
-            Console.ResetColor();
-        }
-
-        /*
-            Ref.: https://stackoverflow.com/a/11495179
-         */
-        private static ConsoleKey GetConsoleKeyFromString(string tecla)
-        {
-            ConsoleKey ck;
-
-            Enum.TryParse<ConsoleKey>(tecla, out ck);
-
-            return ck;
-        }
-
-        public static void Exit()
-        {
-            Console.Clear();
-
-            Console.WriteLine("\n\n\tHasta pronto {0}!\n", NombreUsuario);
-
-            System.Environment.Exit(0);
         }
     }
 }
